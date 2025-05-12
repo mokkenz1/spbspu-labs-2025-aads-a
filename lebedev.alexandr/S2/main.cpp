d1f83ff7a7686d9ca2b2070b6129b6ab7eda6a8c
mokkenz1 <lam1600@mail.ru>
2025-05-12 04:06:45 +0300

    Output fix

--- lebedev.alexandr/S2/main.cpp
+++ lebedev.alexandr/S2/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
     res.pop();
     while (!res.empty())
     {
-      std::cout << res.top();
+      std::cout << ' ' << res.top();
       res.pop();
     }
     std::cout << '\n';
